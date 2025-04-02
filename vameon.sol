22134db9b576b4a4af54bb250b4e483f437d50ce
Mr.Ae57 <92690015+Davidkia7@users.noreply.github.com>
2025-04-02 14:54:52 +0700

    Create vameon.sol

---
+++ vameon.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/metatx/ERC2771Context.sol";
+import "./FeeCollector.sol";
+
+contract Vameon is ERC20, ERC20Permit, ERC2771Context, FeeCollector {
+  error InvalidFee();
+
+  constructor(
+    address _trustedForwarder,
+    address _tokensHolder,
+    address _feeCollector
+  )
+    ERC20("Vameon", "VON")
+    ERC20Permit("Vameon")
+    ERC2771Context(_trustedForwarder)
+    FeeCollector(_feeCollector)
+  {
+    _mint(_tokensHolder, 1_000_000_000_000 * 10 ** decimals());
+  }
+
+  function _msgSender()
+    internal
+    view
+    override(Context, ERC2771Context)
+    returns (address sender)
+  {
+    sender = ERC2771Context._msgSender();
+  }
+
+  function _msgData()
+    internal
+    view
+    override(Context, ERC2771Context)
+    returns (bytes calldata)
+  {
+    return ERC2771Context._msgData();
+  }
+
+  function _contextSuffixLength()
+    internal
+    view
+    virtual
+    override(Context, ERC2771Context)
+    returns (uint256)
+  {
+    return ERC2771Context._contextSuffixLength();
+  }
+
+  function transferWithFee(
+    address to,
+    uint256 value,
+    uint256 fee
+  ) public virtual returns (bool) {
+    if (fee == 0) {
+      revert InvalidFee();
+    }
+    address tokenOwner = _msgSender();
+    _transfer(tokenOwner, to, value);
+    _transfer(tokenOwner, getFeeCollector(), fee);
+    return true;
+  }
+}
