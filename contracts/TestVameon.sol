e1fcd51e170c9efbeabd25434aa1d950df0a5974
Mr.Ae57 <92690015+Davidkia7@users.noreply.github.com>
2025-04-02 15:01:19 +0700

    Create TestVameon.sol

---
+++ contracts/TestVameon.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./Vameon.sol";
+
+contract TestVameon {
+    Vameon public vameonToken;
+
+    constructor(address _vameonToken) {
+        vameonToken = Vameon(_vameonToken);
+    }
+
+    // Fungsi untuk menguji transferWithFee
+    function testTransferWithFee(address to, uint256 value, uint256 fee) external {
+        vameonToken.transferWithFee(to, value, fee);
+    }
+
+    // Fungsi untuk memeriksa saldo
+    function getBalance(address account) external view returns (uint256) {
+        return vameonToken.balanceOf(account);
+    }
+}
